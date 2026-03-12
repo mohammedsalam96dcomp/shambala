2c2b62ed00cd4dde4a85da0c96d12ae9ff1e970d
mohammedsalam96dcomp <mohammedfaisalsalam@gmail.com>
2026-03-11 23:01:09 -0400

    frlg to triplelayer

--- triple_layer_converter.py
+++ triple_layer_converter.py
@@ -3,18 +3,36 @@ import io
 import argparse
 import struct
 
-layer_type_mask = 0xF000
-layer_type_shift = 12
+version_emerald = 'emerald'
+version_firered = 'firered'
 
-parser = argparse.ArgumentParser(description='Convert pokeemerald metatiles to use the triple layer system.' )
+parser = argparse.ArgumentParser(description='Convert pokeemerald or pokefirered metatiles to use the triple layer system.' )
 parser.add_argument('--tsroot', required=True,
-                    help='Path to the tilesets directory in your pokeemerald project, e.g. /path/to/pokeemerald/data/tilesets')
+                    help='Path to the tilesets directory in your pokeemerald/pokefirered project, e.g. /path/to/pokeemerald/data/tilesets')
+parser.add_argument('--version', required=True, help = f'Game version: {version_emerald} or {version_firered}')
 
 args = parser.parse_args()
 
 if not os.path.exists(args.tsroot):
     print(f"Given tilesets root directory does not exist: {args.tsroot}")
     exit(1)
+
+if args.version != version_emerald and args.version != version_firered:
+    print(f"Game version '{args.version}' does not exist")
+    exit(1)
+
+if args.version == version_firered:
+    layer_type_mask  = 0x60000000
+    layer_type_shift = 29
+    metatiles_size_factor = 4
+    attribute_size = 4
+    attribute_format = 'I'
+else:
+    layer_type_mask  = 0xF000
+    layer_type_shift = 12
+    metatiles_size_factor = 8
+    attribute_size = 2
+    attribute_format = 'H'
 
 primary_path = os.path.join(args.tsroot, 'primary')
 secondary_path = os.path.join(args.tsroot, 'secondary')
@@ -43,18 +61,18 @@ for tileset_dir in tileset_dirs:
     if not os.path.exists(metatile_attributes_path):
         print(f"[SKIP] {tileset_name} skipped because metatile_attributes.bin was not found.")
         continue
-    if os.path.getsize(metatiles_path) != 8*os.path.getsize(metatile_attributes_path):
-        print(f"[SKIP] {tileset_name} skipped because metatiles.bin is not eight times the size of metatile_attributes.bin (already converted?)")
+    if os.path.getsize(metatiles_path) != metatiles_size_factor*os.path.getsize(metatile_attributes_path):
+        print(f"[SKIP] {tileset_name} skipped because metatiles.bin is not {metatiles_size_factor} times the size of metatile_attributes.bin (already converted?)")
         continue
 
     layer_types = []
     meta_attributes = []
     with open(metatile_attributes_path, 'rb') as fileobj:
-        for chunk in iter(lambda: fileobj.read(2), ''):
+        for chunk in iter(lambda: fileobj.read(attribute_size), ''):
             if chunk == b'':
                 break
-            metatile_attribute = struct.unpack('<H', chunk)[0]
-            meta_attributes.append(metatile_attribute & 0x0FFF)
+            metatile_attribute = struct.unpack(f'<{attribute_format}', chunk)[0]
+            meta_attributes.append(metatile_attribute & ~layer_type_mask)
             layer_types.append((metatile_attribute & layer_type_mask) >> layer_type_shift)
     i = 0
     new_metatile_data = []
@@ -78,7 +96,7 @@ for tileset_dir in tileset_dirs:
             i += 1
 
     metatile_buffer = struct.pack(f'<{len(new_metatile_data)}H', *new_metatile_data)
-    metatile_attribute_buffer = struct.pack(f'<{len(meta_attributes)}H', *meta_attributes)
+    metatile_attribute_buffer = struct.pack(f'<{len(meta_attributes)}{attribute_format}', *meta_attributes)
     with open(metatiles_path, 'wb') as fileobj:
         fileobj.write(metatile_buffer)
     with open(metatile_attributes_path, 'wb') as fileobj:
